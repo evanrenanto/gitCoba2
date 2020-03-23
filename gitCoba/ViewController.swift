9b964617f3bde19a026c60a7f1247e81942ee5e2
evanrenanto <62125175+evanrenanto@users.noreply.github.com>
2020-03-23 12:40:20 +0700

    Initial Commit

---
+++ gitCoba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitCoba
+//
+//  Created by Evan Renanto on 23/03/20.
+//  Copyright © 2020 Evan Renanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
